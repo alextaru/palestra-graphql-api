b4ced3e0c121ea187b9a4982e7abdef964b825d2
alexandre goncalves <alex.taru@yahoo.com.br>
2022-02-25 11:20:52 -0300

    fix: declarar http

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const router = require('./routers');
 const app = express();
 app.use(cors());
 app.use(router);
 const server = http.createServer(app);
 server.listen(3333);
-console.log('Running a GraphQL API server at http://localhost:3333/playground');
+console.log('Running API server at http://localhost:3333');
